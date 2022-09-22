13e340348d1fac2a36af005f03500604fbcb2a24
YellowDog <33691422+YellowDogMachine@users.noreply.github.com>
2022-09-22 15:27:01 +0000

    Release 3.6.15

--- src/yellowdog_client/model/operating_system_licence.py
+++ src/yellowdog_client/model/operating_system_licence.py
@@ -2,8 +2,14 @@ from enum import Enum
 
 
 class OperatingSystemLicence(Enum):
-    WINDOWS = "WINDOWS"
-    NONE = "NONE"
+    WINDOWS = "WINDOWS", "Windows"
+    NONE = "NONE", "None"
+
+    def __new__(cls, value, display_name: str):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.display_name = display_name
+        return obj
 
     def __str__(self) -> str:
         return self.name
--- src/yellowdog_client/model/usage_type.py
+++ src/yellowdog_client/model/usage_type.py
@@ -2,8 +2,14 @@ from enum import Enum
 
 
 class UsageType(Enum):
-    ON_DEMAND = "ON_DEMAND"
-    SPOT = "SPOT"
+    ON_DEMAND = "ON_DEMAND", "On Demand"
+    SPOT = "SPOT", "Spot"
+
+    def __new__(cls, value, display_name: str):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.display_name = display_name
+        return obj
 
     def __str__(self) -> str:
         return self.name
